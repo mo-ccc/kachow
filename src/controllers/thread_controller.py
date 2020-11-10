62c339a11f14c75ef62f9e2955c62f05ffca5ee8
Mo <css012002@coderacademy.edu.au>
2020-11-10 17:45:00 +1100

    updated swagger. added post to thread controller

--- src/controllers/thread_controller.py
+++ src/controllers/thread_controller.py
@@ -2,6 +2,7 @@ import flask
 from main import db
 from models.Thread import Thread
 from schemas.Thread_Schema import thread_schema, threads_schema
+from sqlalchemy import func
 
 thread = flask.Blueprint('thread', __name__, url_prefix='/threads')
 
@@ -10,3 +11,18 @@ def get_threads():
     all_threads = Thread.query.all()
     output = threads_schema.dump(all_threads)
     return flask.jsonify(output)
+    
+@thread.route('/', methods=['POST'])
+def create_thread():
+    response = flask.request.json
+    data = thread_schema.load(response)
+    new_thread = Thread()
+    new_thread.author_id = data["author_id"]
+    new_thread.category_id = data["category_id"]
+    new_thread.title = data["title"]
+    new_thread.status = data["status"]
+    new_thread.time_created = func.now()
+    db.session.add(new_thread)
+    db.session.commit()
+    return flask.jsonify(threads_schema.dump(Thread.query.all()))
+    
--- src/database.py
+++ src/database.py
@@ -1,13 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
+import psycopg2
 import os
 import dotenv
 
 dotenv.load_dotenv()
 def init_db(app):
-    app.config["SQLALCHEMY_DATABASE_URI"] = 
-        f"postgresql+psycopg2://{os.getenv('DB_USER')}:"
-        f"{os.getenv('DB_PASS')}@"
-        f"{os.getenv('DB_HOST')}:5432/{os.getenv('DB_NAME')}"
+    connection = f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@{os.getenv('DB_HOST')}:5432/{os.getenv('DB_NAME')}"
+    app.config["SQLALCHEMY_DATABASE_URI"] = connection
+        
 
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db = SQLAlchemy(app)
