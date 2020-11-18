0faa79e896e80ff8a3d8754b85a413dd5443cf30
Mo <css012002@coderacademy.edu.au>
2020-11-18 18:46:28 +1100

    added get method for attachment

--- src/controllers/attachment_controller.py
+++ src/controllers/attachment_controller.py
@@ -6,16 +6,16 @@ from pathlib import Path
 import json
 import uuid
 
-attachments = flask.Blueprint('attachments', __name__)
+attachments = flask.Blueprint('attachments', __name__, url_prefix='/attachments')
 
-@attachments.route('/attachments', methods=['POST'])
+@attachments.route('/', methods=['POST'])
 def create_attachment():
     response = flask.request.form["json"]
     jresponse = json.loads(response)
     
     data = attachment_schema.load(jresponse)
     if 'file' not in flask.request.files:
-        return flask.abort(400, description="missing file")
+        return flask.abort(400, description="no file in body")
     file = flask.request.files['file']
     extension = Path(file.filename).suffix
     
@@ -25,8 +25,22 @@ def create_attachment():
     
     new_attachment = Attachment()
     new_attachment.post_id = data["post_id"]
-    new_attachment.attachment_path = file_location
+    new_attachment.attachment_path = f"{unique_id}{extension}"
     new_attachment.post_position = data["post_position"]
+    
+    db.session.add(new_attachment)
+    db.session.commit()
     
     return flask.jsonify(attachment_schema.dump(new_attachment))
     
+@attachments.route('/<int:id>', methods=['GET'])
+def get_attachment_details(id):
+    file = Attachment.query.get(id)
+    if not file:
+        return flask.abort(400, description="missing file")
+    data = attachment_schema.dump(file)
+    return flask.jsonify(data)
+    
+@attachments.route('/media/<string:unique_id>', methods=['GET'])
+def get_attachment(unique_id):
+    pass
